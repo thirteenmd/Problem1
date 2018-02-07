7e84198f246ba7976e590e1c36b5f7a835a97f38
thirteenmd <andreacojocaru@gmail.com>
2018-02-07 17:49:51 +0200

    second commit

--- src/ReadXML.java
+++ src/ReadXML.java
@@ -1,21 +1,86 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
+import static java.nio.file.FileVisitResult.CONTINUE;
+
 public class ReadXML {
-    File folder = new File("{FilePath}");
-    File[] listOfFiles = folder.listFiles();
-    int numberOfFiles = listOfFiles.length;
+    public static class Finder
+            extends SimpleFileVisitor<Path> {
 
-    ArrayList XMLfiles = new ArrayList<String>();
+        public ArrayList<Path> xmlFiles = new ArrayList<Path>();
 
-    private void FindFiles(){
-        for(int i = 0; i < numberOfFiles; i++){
-            String filename = listOfFiles[i].getName();
-            if (filename.endsWith(".xml") || filename.endsWith(".XML")){
-                System.out.println(filename);
-                XMLfiles.add(filename);
+        private final PathMatcher matcher;
+        private int numMatches = 0;
+
+        Finder(String pattern) {
+            matcher = FileSystems.getDefault()
+                    .getPathMatcher("glob:" + pattern);
+        }
+
+        // Compares the glob pattern against
+        // the file or directory name.
+        void find(Path file) {
+            Path name = file.getFileName();
+            if (name != null && matcher.matches(name)) {
+                numMatches++;
+                System.out.println(file);
+                xmlFiles.add(name);
             }
         }
 
+        // Prints the total number of
+        // matches to standard out.
+        void done() {
+            System.out.println("Matched: "
+                    + numMatches);
+        }
+
+        // Invoke the pattern matching
+        // method on each file.
+        @Override
+        public FileVisitResult visitFile(Path file,
+                                         BasicFileAttributes attrs) {
+            find(file);
+            return CONTINUE;
+        }
+
+        // Invoke the pattern matching
+        // method on each directory.
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir,
+                                                 BasicFileAttributes attrs) {
+            find(dir);
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file,
+                                               IOException exc) {
+            System.err.println(exc);
+            return CONTINUE;
+        }
+    }
+
+    static void usage() {
+        System.err.println("java Find <path>" +
+                " -name \"<glob_pattern>\"");
+        System.exit(-1);
+    }
+
+    public static void main(String[] args)
+            throws IOException {
+
+//        if (args.length < 3 || !args[1].equals("-name"))
+//            usage();
+
+        Path startingDir = Paths.get("C:\\Users\\admin\\Documents\\CopyCharacters");
+        String pattern = "*.{xml,XML}";
+
+        Finder finder = new Finder(pattern);
+        Files.walkFileTree(startingDir, finder);
+        finder.done();
     }
 }
